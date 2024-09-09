a1ce962b9987daa1a4ebaf78e4e3886c46b8e41b
lixin09 <lixin09@zhihu.com>
2024-09-09 17:56:15 +0800

    new commit in 2024/09/09

---
+++ NLP/practice_1_chat_robot.py
@@ -0,0 +1,9 @@
+
+
+random_responses = ["That is quite interesting, please tell me more.",
+                    "I see. Do go on.",
+                    "Why do you say that?",
+                    "Funny weather we've been having, isn't it?",
+                    "Let's change the subject.",
+                    "Did you catch the game last night?"]
+
